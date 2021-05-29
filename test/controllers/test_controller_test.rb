49e105c5d730025379a22727791b775a258af6b4
cwarlen-dice <sevens_lovers@yahoo.co.jp>
2021-05-29 20:04:00 +0900

    rubocop導入

--- test/controllers/test_controller_test.rb
+++ test/controllers/test_controller_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
 class TestControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get test_index_url
     assert_response :success
   end
-
 end
