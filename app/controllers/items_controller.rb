fe22ea86fccc4bd789a0c8f9740bd0253b2ace5c
cwarlen-dice <sevens_lovers@yahoo.co.jp>
2021-06-03 21:18:40 +0900

    ロックアウト修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: %i[index]
+  before_action :no_user, except: %i[index]
+  before_action :set_user, except: %i[index]
   before_action :check_user, except: %i[index show]
 
   def index
@@ -20,6 +22,7 @@ class ItemsController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:user_id])
     @item = Item.find(params[:id])
     impressionist(@item) # PVカウントアップ
   end
@@ -31,6 +34,14 @@ class ItemsController < ApplicationController
   end
 
   def check_user
-    redirect_to(root_path) and return if params[:user_id].nil? && (current_user.id != params[:user_id].to_i)
+    redirect_to(root_path) and return if current_user.id != @user.id
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def no_user
+    redirect_to(root_path) if User.find_by(id: params[:user_id]).nil?
   end
 end
