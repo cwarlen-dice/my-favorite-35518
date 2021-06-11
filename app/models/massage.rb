ead5730cf31c85e38b91e3035c455f9034c350d2
cwarlen-dice <sevens_lovers@yahoo.co.jp>
2021-06-11 10:44:00 +0900

    モデル：アソシエーション設定

--- app/models/massage.rb
+++ app/models/massage.rb
@@ -1,2 +1,11 @@
 class Massage < ApplicationRecord
+  belongs_to :room
+  belongs_to :user
+  has_one_attached :image
+
+  validates :content, presence: true, unless: :was_attached?
+
+  def was_attached?
+    image.attached?
+  end
 end
